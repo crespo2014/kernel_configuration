63dc7b179f11548428ede0a0b0078037dd3fb55e
lester.crespo <lester.crespo.es@gmail.com>
2015-06-09 13:10:50 +0100

    added missing files

---
+++ utils/linux/string.h
@@ -0,0 +1,15 @@
+/*
+ * string.h
+ *
+ *  Created on: 9 Jun 2015
+ *      Author: lester.crespo
+ */
+
+#ifndef UTILS_LINUX_STRING_H_
+#define UTILS_LINUX_STRING_H_
+
+
+#include <string.h>
+
+
+#endif /* UTILS_LINUX_STRING_H_ */
